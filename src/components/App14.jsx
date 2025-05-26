bbf0d7ed0146d85b28e72cd251734ad010fbbab2
Medatarunvenkat <tarunvm0909@gmail.com>
2025-05-26 19:09:27 +0530

    done

--- src/components/App14.jsx
+++ src/components/App14.jsx
@@ -1,10 +1,10 @@
-import React from "react";
-import { useState ,useEffect} from "react";
 import axios from "axios";
+import React, { useEffect, useState } from "react";
 function App14(){
     const [users,setUsers]=useState([])
+    const [search,setSearch]=useState("")
     const fetchData=async()=>{
-        const url="https://jsonplaceholder.typicode.com/posts";
+        const url="https://jsonplaceholder.typicode.com/users";
         const res=await axios.get(url);
         setUsers(res.data);
     }  
@@ -12,13 +12,22 @@ function App14(){
     useEffect(()=>{
        fetchData(); 
     },[])
+
+    const names=users.filter((user)=>
+        user.name.includes(search.toLowerCase())
+    );
+    
     return(
         <div>
             <h1>App14</h1>
             <h2>useEffect hook to fetch api</h2>
+            <p>
+                <input type="text" placeholder="search" value={search} onChange={(e)=>setSearch(e.target.value)}/>
+
+            </p>
             <ul>
-                {users.map((user)=>(
-                    <li key={user.id}>{user.title}</li>
+                {names.map((user)=>(
+                    <li key={user.id}>{user.name}</li>
                 ))}
             </ul>
         </div>
