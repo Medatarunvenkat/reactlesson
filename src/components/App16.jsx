2ca14f6698a578a6c2725acdf18ee57723ac041f
Medatarunvenkat <tarunvm0909@gmail.com>
2025-05-26 20:13:46 +0530

    app16 now

--- src/components/App16.jsx
+++ src/components/App16.jsx
@@ -1,15 +1,13 @@
 import React, { useRef, useState } from "react";
 
 function App16() {
-  const prevValueRef = useRef(0);
-  const [value, setValue] = useState(0);
+  const prevValueRef=useRef(0);
+  const [value, setValue]=useState(0);
 
-  const handleChange = (e) => {
-    setValue(Number(e.target.value));
-  };
-
-  const handleSubmit = () => {
-    prevValueRef.current = value;
+  const handleChange=(e) => {
+    const newValue=Number(e.target.value);
+    prevValueRef.current=value;
+    setValue(newValue);
   };
 
   return (
@@ -17,9 +15,8 @@ function App16() {
       <h1>App16</h1>
       <h2>useRef to store previous number</h2>
       <input type="number" onChange={handleChange} />
-      <button onClick={handleSubmit}>Submit</button>
-      <p>Previous value: {prevValueRef.current}</p>
-      <p>Current value: {value}</p>
+      <p>Previous value:{prevValueRef.current}</p>
+      <p>Current value:{value}</p>
     </div>
   );
 }
